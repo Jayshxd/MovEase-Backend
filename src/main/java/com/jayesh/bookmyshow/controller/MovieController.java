b98fdcbf1fb2752b2fca7df9c60d6bf8315117d9
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-10-14 01:33:35 +0530

    feat : Made Basic Crud APIs for Movie.java

---
+++ src/main/java/com/jayesh/bookmyshow/controller/MovieController.java
@@ -0,0 +1,58 @@
+package com.jayesh.bookmyshow.controller;
+
+import com.jayesh.bookmyshow.dto.request.MovieRequestDto;
+import com.jayesh.bookmyshow.dto.response.MovieResponseDto;
+import com.jayesh.bookmyshow.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<MovieResponseDto>> get() {
+        List<MovieResponseDto> res = movieService.findMovies();
+        return ResponseEntity.ok(res);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<MovieResponseDto> post(@RequestBody MovieRequestDto req) {
+        MovieResponseDto res = movieService.createMovie(req);
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MovieResponseDto> put(@PathVariable Long id, @RequestBody MovieRequestDto req) {
+        MovieResponseDto res = movieService.updateByPut(id,req);
+        return ResponseEntity.ok(res);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        movieService.deleteMovie(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieResponseDto> getMovie(@PathVariable Long id) {
+        MovieResponseDto res = movieService.getMovieById(id);
+        return ResponseEntity.ok(res);
+
+    }
+
+    @PostMapping("/bulk")
+    public ResponseEntity<List<MovieResponseDto>> post(@RequestBody List<MovieRequestDto> req) {
+        List<MovieResponseDto> res = movieService.createMoviesInBulk(req);
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+
+}
