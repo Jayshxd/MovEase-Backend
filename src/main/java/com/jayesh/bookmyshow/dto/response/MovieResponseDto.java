f6d993d0a2cc39bf32234ecaf5cbb7ca0c9b7696
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-10-14 18:22:11 +0530

    refactor : changed the response now more details are being shown

--- src/main/java/com/jayesh/bookmyshow/dto/response/MovieResponseDto.java
+++ src/main/java/com/jayesh/bookmyshow/dto/response/MovieResponseDto.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -14,11 +16,17 @@ public class MovieResponseDto {
     private Long id;
     private String title;
     private String duration;
+    private String genre;
+    private LocalDate releaseDate;
+    private String language;
 
     public MovieResponseDto(Movie movie) {
         this.id = movie.getId();
         this.title = movie.getTitle();
         this.duration = movie.getDuration();
+        this.genre = movie.getGenre();
+        this.releaseDate = movie.getReleaseDate();
+        this.language = movie.getLanguage();
 
     }
 }
